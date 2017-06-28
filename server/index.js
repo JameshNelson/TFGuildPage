3dd2debde93ac1fd7c4551acff4ebe5f8a88cc80
JameshNelson <jamesh.nelson@att.net>
2017-06-27 18:29:48 -0600

    db connected

--- server/index.js
+++ server/index.js
@@ -6,20 +6,26 @@ const express = require('express'),
   config = require('./config.js'),
   app = express();
 
-  // var massiveInst = massive.connectSync({
-  //     connectionString: `postgress://${config.NAME}:${config.PASSWORD}@${config.ENDPOINT}:5432/${config.DBPORT}`
-  // });
-  // app.set('db', massiveInst);
-  // var db = app.get('db');
+  var massiveInst = massive.connectSync({
+      connectionString: config.ENDPOINT
+  });
+  app.set('db', massiveInst);
+  var db = app.get('db');
 
   app.use(bodyParser.json());
   app.use(express.static('./dist'));
   app.use(cors());
-  // app.use(session({
-  //   secret: config.SESSION_SECRET,
-  //   saveUnitialized: false,
-  //   resave: false,
-  // }))
+  app.use(session({
+    secret: config.SESSION_SECRET,
+    saveUnitialized: false,
+    resave: false,
+  }))
+
+  app.get('/roster', function(req, res, next){
+    db.get_roster(function(err, result){
+      res.send(result);
+    })
+  })
 
 app.listen(3000, function(){
   console.log('Running on Port ' + 3000 + ' me lord')
